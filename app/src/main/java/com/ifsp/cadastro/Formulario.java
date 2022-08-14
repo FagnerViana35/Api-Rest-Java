5d8f4c2b398cf7acc3cbe1e76f1a63da005e319f
Fagner Viana <fagner.viana@domvsit.com.br>
2022-08-14 14:04:06 -0300

    Foi adicionado a classe a Formulário

---
+++ app/src/main/java/com/ifsp/cadastro/Formulario.java
@@ -0,0 +1,91 @@
+package com.ifsp.cadastro;
+
+public class Formulario {
+
+    private String name;
+    private String phone;
+    private String email;
+    private boolean checkoutEmails;
+    private String sexo;
+    private String city;
+    private String uf;
+
+    public Formulario(String name, String phone, String email, boolean checkoutEmails, String sexo, String city, String uf) {
+        this.name = name;
+        this.phone = phone;
+        this.email = email;
+        this.checkoutEmails = checkoutEmails;
+        this.sexo = sexo;
+        this.city = city;
+        this.uf = uf;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public boolean isCheckoutEmails() {
+        return checkoutEmails;
+    }
+
+    public void setCheckoutEmails(boolean checkoutEmails) {
+        this.checkoutEmails = checkoutEmails;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getUf() {
+        return uf;
+    }
+
+    public void setUf(String uf) {
+        this.uf = uf;
+    }
+
+    @Override
+    public String toString() {
+        return "Formulario{" +
+                "name='" + name + '\'' +
+                ", phone='" + phone + '\'' +
+                ", email='" + email + '\'' +
+                ", checkoutEmails=" + checkoutEmails +
+                ", sexo='" + sexo + '\'' +
+                ", city='" + city + '\'' +
+                ", uf='" + uf + '\'' +
+                '}';
+    }
+}
--- app/src/main/java/com/ifsp/cadastro/MainActivity.java
+++ app/src/main/java/com/ifsp/cadastro/MainActivity.java
@@ -3,12 +3,69 @@ package com.ifsp.cadastro;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+
+import com.ifsp.cadastro.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding activityMainBinding;
+
+    private String name;
+    private String phone;
+    private String email;
+    private boolean checkoutEmails;
+    private String sexo;
+    private String city;
+    private String uf;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        activityMainBinding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(activityMainBinding.getRoot());
+
+        activityMainBinding.inputNameFull.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {}
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                name = charSequence.toString();
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {}
+        });
+
+        activityMainBinding.inputPhone.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {}
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                phone = charSequence.toString();
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {}
+        });
+
+        activityMainBinding.inputEmail.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {}
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                email = charSequence.toString();
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {}
+        });
     }
+
+
 }
